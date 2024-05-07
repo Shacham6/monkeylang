9a60ab3fc8c2279e384b0fd02995cd705a62b73f
Shacham Ginat <shacham.ginat@pagaya.com>
2024-05-07 09:03:50 +0300

    moving things

--- parser/parser.go
+++ parser/parser.go
@@ -8,49 +8,15 @@ import (
 	"strconv"
 )
 
-type Precedence int
-
-const (
-	_                      = iota
-	LOWEST      Precedence = iota
-	EQUALS      Precedence = iota // == or !=
-	LESSGREATER Precedence = iota // > or <
-	SUM         Precedence = iota // - or +
-	PRODUCT     Precedence = iota // / (slash) or *
-	PREFIX      Precedence = iota // -X or !X
-	CALL        Precedence = iota // myFunction(X)
-)
-
-var precedences = map[token.TokenType]Precedence{
-	token.EQ:      EQUALS,
-	token.NOT_EQ:  EQUALS,
-	token.LT:      LESSGREATER,
-	token.GT:      LESSGREATER,
-	token.PLUS:    SUM,
-	token.MINUS:   SUM,
-	token.SLASH:   PRODUCT,
-	token.ASTERIX: PRODUCT,
-}
-
-func (p *Parser) peekPrecedence() Precedence {
-	pr, ok := precedences[p.peekToken.Type]
-	if !ok {
-		return LOWEST
-	}
-	return pr
-}
-
-func (p *Parser) curPrecedence() Precedence {
-	pr, ok := precedences[p.curToken.Type]
-	if !ok {
-		return LOWEST
-	}
-	return pr
-}
-
 type prefixParseFn func() ast.Expression
 
 type infixParseFn func(ast.Expression) ast.Expression
+
+func Parse(input string) (*ast.Program, []string) {
+	p := New(lexer.New(input))
+	program := p.ParseProgram()
+	return program, p.errors
+}
 
 type Parser struct {
 	l *lexer.Lexer
