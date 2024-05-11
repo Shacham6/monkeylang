eef5013ebe1e3854e3cc0af1fbde37797e4676d6
Shacham Ginat <shacham.ginat@pagaya.com>
2024-05-11 12:06:47 +0300

    breaking but will restore

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -376,7 +376,7 @@ func TestParsingPrefixExpressions(t *testing.T) {
 	}
 }
 
-func TestParsingInfixExpressions(t *testing.T) {
+func TestParsingInfixInt64Expressions(t *testing.T) {
 	infixTests := []struct {
 		input      string
 		leftValue  int64
@@ -391,25 +391,33 @@ func TestParsingInfixExpressions(t *testing.T) {
 		{"5 < 5;", 5, "<", 5},
 		{"5 == 5;", 5, "==", 5},
 		{"5 != 5;", 5, "!=", 5},
+		// {"true == true", true, "==", true},
+		// {"true != false", true, "!=", false},
+		// {"false == false", false, "==", false},
 	}
 
-	for _, infixTest := range infixTests {
-		p := parser.New(lexer.New(infixTest.input))
-		program := p.ParseProgram()
-		checkParserErrors(t, p)
+	for _, tt := range infixTests {
+		t.Run(
+			tt.input,
+			func(t *testing.T) {
+				p := parser.New(lexer.New(tt.input))
+				program := p.ParseProgram()
+				checkParserErrors(t, p)
 
-		if len(program.Statements) != 1 {
-			t.Fatalf("len(program.Statements). got = %d. expect = %d", len(program.Statements), 1)
-		}
+				if len(program.Statements) != 1 {
+					t.Fatalf("len(program.Statements). got = %d. expect = %d", len(program.Statements), 1)
+				}
 
-		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
-		if !ok {
-			t.Fatalf("program.Statements[0] is not ExpressionStatement, got = %T", program.Statements[0])
-		}
+				stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+				if !ok {
+					t.Fatalf("program.Statements[0] is not ExpressionStatement, got = %T", program.Statements[0])
+				}
 
-		if !testInfixExpression(t, stmt.Expression, infixTest.leftValue, infixTest.operator, infixTest.rightValue) {
-			return
-		}
+				if !testInfixExpression(t, stmt.Expression, tt.leftValue, tt.operator, tt.rightValue) {
+					return
+				}
+			},
+		)
 	}
 }
 
