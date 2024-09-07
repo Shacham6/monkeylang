8af04168162cc1da961ef0c8ad8cb0478b235420
Shacham Ginat <shacham.ginat@pagaya.com>
2024-09-07 11:15:43 +0300

    Merge definitions file into opcodes
    commit-id:1bea2b2e

--- code/opcodes.go
+++ code/opcodes.go
@@ -1,5 +1,27 @@
 package code
 
+import "fmt"
+
 const (
 	OpConstant Opcode = iota
+	OpAdd
 )
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+	OpAdd:      {"OpAdd", []int{}},
+}
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+
+	return def, nil
+}
