5c728d5108b4d72c6a8ebd6dc46ccdd933caa489
Shacham Ginat <shacham.ginat@pagaya.com>
2024-09-19 11:05:51 +0300

    Remove fixme comment

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -9,6 +9,6 @@ func TestIntegerArithmetic(t *testing.T) {
 	vmtest.RunVmTests(t, []vmtest.VmTestCase{
 		vmtest.New("1", 1),
 		vmtest.New("2", 2),
-		vmtest.New("1 + 2", 3), // FIXME
+		vmtest.New("1 + 2", 3),
 	})
 }
