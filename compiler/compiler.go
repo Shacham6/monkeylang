46fb352fff3d0304bba117e327cfa05aba0d0a75
Shacham Ginat <shacham6@gmail.com>
2024-10-03 08:25:56 +0300

    Saving previous instructions

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -7,15 +7,26 @@ import (
 	"monkey/object"
 )
 
+type EmittedInstruction struct {
+	Opcode   code.Opcode
+	Position int
+}
+
 type Compiler struct {
 	instructions code.Instructions
 	constants    []object.Object
+
+	lastInstruction EmittedInstruction
+	prevInstruction EmittedInstruction
 }
 
 func New() *Compiler {
 	return &Compiler{
 		instructions: code.Instructions{},
 		constants:    []object.Object{},
+
+		lastInstruction: EmittedInstruction{},
+		prevInstruction: EmittedInstruction{},
 	}
 }
 
@@ -27,12 +38,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 				return err
 			}
 		}
+		return nil
 
 	case *ast.ExpressionStatement:
 		if err := c.Compile(node.Expression); err != nil {
 			return err
 		}
 		c.emit(code.OpPop)
+		return nil
 
 	case *ast.InfixExpression:
 		// this operator is treated as equivalent to others *just with flipped operands*.
@@ -75,6 +88,7 @@ func (c *Compiler) Compile(node ast.Node) error {
 		default:
 			return fmt.Errorf("infix operator %s not supported", node.Operator)
 		}
+		return nil
 
 	case *ast.Boolean:
 		if node.Value() {
@@ -82,10 +96,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		} else {
 			c.emit(code.OpFalse)
 		}
+		return nil
 
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(integer))
+		return nil
 
 	case *ast.PrefixExpression:
 		if err := c.Compile(node.Right); err != nil {
@@ -100,11 +116,38 @@ func (c *Compiler) Compile(node ast.Node) error {
 		default:
 			panic(fmt.Sprintf("prefix operator %s is not supported", node.Operator))
 		}
+		return nil
+
+	case *ast.IfExpression:
+		if err := c.Compile(node.Condition()); err != nil {
+			return err
+		}
+
+		// Shacham:
+		// I'll follow instructions for now, which consists of filling in bogus offsets
+		// and correcting them later. But I still wonder about the merits of compiling
+		// these on _new_ instance of Compiler, copying the data with "corrected" offset.
+		// The reason I tend to maybe like _that_ sightly more is parallelization potential.
+
+		// Emit the opcode with a bogus offset
+		_ = c.emit(code.OpJumpNotTruthy, 9999)
+		if err := c.Compile(node.Consequence()); err != nil {
+			return err
+		}
+
+		return nil
+
+	case *ast.BlockStatement:
+		for _, s := range node.Statements() {
+			if err := c.Compile(s); err != nil {
+				return err
+			}
+		}
+		return nil
 
 	default:
 		panic(fmt.Sprintf("don't support node of type %T", node))
 	}
-	return nil
 }
 
 func (c *Compiler) Bytecode() *Bytecode {
@@ -122,7 +165,18 @@ func (c *Compiler) addConstant(obj object.Object) int {
 func (c *Compiler) emit(op code.Opcode, operands ...int) int {
 	ins := code.Make(op, operands...)
 	pos := c.addInstruction(ins)
+	c.updateLastInstruction(op, pos)
 	return pos
+}
+
+func (c *Compiler) updateLastInstruction(op code.Opcode, pos int) {
+	previous := c.lastInstruction
+	last := EmittedInstruction{
+		Opcode:   op,
+		Position: pos,
+	}
+	c.prevInstruction = previous
+	c.lastInstruction = last
 }
 
 func (c *Compiler) addInstruction(ins []byte) int {
