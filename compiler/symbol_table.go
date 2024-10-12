5e27c6f7d786b225e100f91a16ae981ff46b29b7
Shacham Ginat <shacham6@gmail.com>
2024-10-12 11:59:20 +0300

    Implement symbol table methods, add tests

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -12,6 +12,10 @@ type Symbol struct {
 	Index int
 }
 
+func NewSymbol(name string, scope SymbolScope, index int) Symbol {
+	return Symbol{name, scope, index}
+}
+
 type SymbolTable struct {
 	store         map[string]Symbol
 	numDefintions int
@@ -20,4 +24,16 @@ type SymbolTable struct {
 func NewSymbolTable() *SymbolTable {
 	s := map[string]Symbol{}
 	return &SymbolTable{s, 0}
+}
+
+func (s *SymbolTable) Define(name string) Symbol {
+	symbol := NewSymbol(name, GlobalScope, s.numDefintions)
+	s.store[name] = symbol
+	s.numDefintions++
+	return symbol
+}
+
+func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
+	symbol, ok := s.store[name]
+	return symbol, ok
 }
