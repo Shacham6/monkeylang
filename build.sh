2dfdd07becff50334fc2db5b4d641a955139f975
Shacham Ginat <shachamginat@Shachams-MacBook-Pro.local>
2025-05-03 12:39:23 +0300

    push into build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
+_help() {
+  cat <<EOT
+Usage: ./build.sh <command>
+
+commands:
+    test              run the tests (containarized using earthly)
+    test.l, testl     run the tests (locally)
+    build             build the artifacts (contanarized using earthly)
+    build.l, buildl   build the artifacts (locally)
+    help              print this message
+EOT
+}
+
+xx() {
+  echo ">>" ${*}
+  ${*}
+}
+
 case "${1}" in
-  test) earthly +test ;;
-  test.l) make test ;;
-  build) earthly +build ;;
+  test) shift; xx earthly +test ${*} ;;
+  test.l|testl) shift; xx go test ./... ${*} ;;
+  build) shift; xx earthly +build ${*} ;;
+  build.l|buildl) shift; xx go build -o targets/ ${*} ;;
+  *|help) _help ;;
 esac
